104fb7b32df48b3f8e0b96036650d7e5e5cc90cc
Ben Weber <bgweber@gmail.com>
2017-05-23 22:31:56 -0700

    Create Java_UCBF.java

---
+++ Recommendations/Java_UCBF.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+ 
+public class UserBased {
+
+	public static void main(String[] args) throws Exception {
+
+		DataModel model = new FileDataModel(new File("Games.csv"));
+		UserSimilarity similarity = new TanimotoCoefficientSimilarity(model);
+		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+		List recommendations = recommender.recommend(101, 5);
+		System.out.println(recommendations);
+		
+	}
+}
